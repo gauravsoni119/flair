006728c8ef9749c0bb9d74d6078e8800b47b8801
sonigaurav119 <sonigaurav119@gmail.com>
2020-02-09 12:06:42 +0100

    refactor(button): add logger to button component
    add logger to button component

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LoggerService } from '../../../../core/src/lib/logger.service';
 
 @Component({
   selector: 'flair-button',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() type: 'primary' | 'secondary';
+
+  constructor(private loggerService: LoggerService) { }
 
   ngOnInit() {
   }
