4e2dc46b4d7af517210e45873b27aa96e25f94de
sonigaurav119 <sonigaurav119@gmail.com>
2020-02-09 22:52:36 +0100

    refactor(*): refactor code

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -10,7 +10,7 @@ export class ButtonComponent implements OnInit {
   @Input() type: 'primary' | 'secondary';
   @Input() class: string;
   @Input() text: string;
-  @Input() icon: string;
+  @Input() icon: string | undefined;
 
   constructor() { }
 
