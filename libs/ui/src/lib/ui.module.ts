e24911093960436dd5e7733c448902569c39e9cb
sonigaurav119 <sonigaurav119@gmail.com>
2020-02-16 12:50:39 +0100

    feat(input): add input componnent
    add input component to lib

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent]
+  declarations: [ButtonComponent, InputComponent]
 })
 export class UiModule {}
