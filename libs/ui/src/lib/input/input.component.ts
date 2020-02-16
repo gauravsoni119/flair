6c5f4398a1bc9714119ebb47e45895d14303acce
sonigaurav119 <sonigaurav119@gmail.com>
2020-02-16 16:54:30 +0100

    feat(*): add option to disable input
    Add option to disable input

--- libs/ui/src/lib/input/input.component.ts
+++ libs/ui/src/lib/input/input.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InputComponent implements OnInit {
 
   @Input() classes: string | undefined;
+  @Input() disable: boolean;
 
   constructor() { }
 
